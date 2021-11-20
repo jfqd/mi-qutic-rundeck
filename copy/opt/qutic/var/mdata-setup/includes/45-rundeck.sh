1890b19842879429120b3cee0efe12c79be3d6cf
jfqd <jfqd@blun.org>
2021-11-20 15:55:58 +0100

    add mysql config

--- copy/opt/qutic/var/mdata-setup/includes/45-rundeck.sh
+++ copy/opt/qutic/var/mdata-setup/includes/45-rundeck.sh
@@ -7,5 +7,27 @@ if mdata-get project_name 1>/dev/null 2>&1; then
     mkdir -p "/opt/rundeck/projects/${PROJECT_NAME}/etc/"
     curl -s -L "${RESOURCES_XML}" > "/opt/rundeck/projects/${PROJECT_NAME}/etc/resources.xml"
   fi
+fi
+
+if mdata-get mysql_host 1>/dev/null 2>&1; then
+  MYSQL_HOST=`mdata-get mysql_host`
+  MYSQL_DB=`mdata-get mysql_database`
+  MYSQL_USER=`mdata-get mysql_user`
+  MYSQL_PWD=`mdata-get mysql_passwd`
+  
+  sed -i \
+      -e "s/dataSource.url/# dataSource.url/" \
+       /opt/rundeck/server/config/rundeck-config.properties
+  
+  cat >> /opt/rundeck/server/config/rundeck-config.properties << EOF
+
+# mysql settings  
+dataSource.driverClassName = com.mysql.jdbc.Driver
+dataSource.url = jdbc:mysql://${MYSQL_HOST}/${MYSQL_DB}?autoReconnect=true&useSSL=false
+dataSource.username = ${MYSQL_USER}
+dataSource.password = ${MYSQL_PWD}
+EOF
+chmod 0640 /opt/rundeck/server/config/rundeck-config.properties
+chown rundeck:rundeck /opt/rundeck/server/config/rundeck-config.properties
 
 fi
